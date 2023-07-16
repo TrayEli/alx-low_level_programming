e15a9e21baaca8f2407f28fd4c1ba55418a3afca
Wisdom Eli Gbordzoe <wisdom.eli7@gmail.com>
2023-07-15 18:19:23 -0700

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Description: Prints the message "and that piece of art is useful" -
+ *              Dora Korpar, 2015-10-19, followed by a new line,
+ *              to the standard error.
+ *
+ * Return: 1 (Failure)
+ */
+int main(void)
+{
+	const char message[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	size_t len = sizeof(message) - 1;
+	write(STDERR_FILENO, message, len);
+
+	return 1;
+}
