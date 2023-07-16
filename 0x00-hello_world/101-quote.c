971b4f3dbbf64bcff1076967ef452c98d41d7ef0
Wisdom Eli Gbordzoe <wisdom.eli7@gmail.com>
2023-07-15 18:27:09 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -12,8 +12,11 @@
  */
 int main(void)
 {
-	const char message[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	const char message[] =
+		"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 	size_t len = sizeof(message) - 1;
+
 	write(STDERR_FILENO, message, len);
+
 	return (1);
 }
