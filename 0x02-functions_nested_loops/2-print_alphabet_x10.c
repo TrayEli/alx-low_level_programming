3532f20574e4b6c1d1f720ab3961b1ef6e1e876b
Wisdom Eli Gbordzoe <wisdom.eli7@gmail.com>
2023-07-18 16:57:03 -0700

    function nested loop

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -5,16 +5,14 @@
  */
 void print_alphabet_x10(void)
 {
-    int i;
-    char letter;
-
-    for (i = 0; i < 10; i++)
-    {
-        for (letter = 'a'; letter <= 'z'; letter++)
-        {
-            _putchar(letter);
-        }
-    }
-
-    _putchar('\n');
+int i;
+char ch;
+for (i = 0; i < 10; i++)
+{
+for (ch = 'a'; ch <= 'z'; ch++)
+{
+_putchar(ch);
+}
+}
+_putchar('\n');
 }
