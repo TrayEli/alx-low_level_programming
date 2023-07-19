b12bf401f3f4ac797ae62824c5e667bfebc1c294
Wisdom Eli Gbordzoe <wisdom.eli7@gmail.com>
2023-07-19 16:58:04 -0700

    function nested loop

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * main - _isalpha - checks for alphabetic character 
+ *
+ * Return: 1 if c is letter, 0 otherwise
+ */
+
+int main(void) 
+{ 
+	int_isalpha(int c) 
+	{                
+		return (( c >= 'a' && c <= 'z' || ( c >= 'A' && c <= 'Z'))                         
+				}                                                       
