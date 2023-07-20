c2944d15a482cf61aeab915ab67da038334040ae
Wisdom Eli Gbordzoe <wisdom.eli7@gmail.com>
2023-07-19 17:38:45 -0700

    function nested loop

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -7,10 +7,15 @@
  *Returns -1 and prints - if n is less than zero
  */
 
-int print_sign(int n) 
-{
-    if (n > 0) 
-    {
-        _putchar('+');
-        return (1);
-    } else if (n == 0)
+int print_sign(int n) {
+    if (n > 0) {
+        printf("+");
+        return 1;
+    } else if (n == 0) {
+        printf("0");
+        return 0;
+    } else {
+        printf("-");
+        return -1;
+    }
+}
