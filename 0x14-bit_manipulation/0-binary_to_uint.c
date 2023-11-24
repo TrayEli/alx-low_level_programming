8dc2e7d72dce4e9ed6a76abea6aa919cdd4ecb88
TrayEli <wisdom.eli7@gmail.com>
2023-11-24 10:18:28 -0800

    bit manipulation

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -9,20 +9,20 @@
  */
 unsigned int binary_to_uint(const char *b) 
 {
-    if (b == NULL)
+	if (b == NULL)
         return 0;
 
-    unsigned int convertedNum = 0;
+    	unsigned int convertedNum = 0;
 
-    while (*b != '\0') 
-    {
+    	while (*b != '\0') 
+    	{
         if (*b != '0' && *b != '1')
-            return 0;
+   	return 0;
 
         convertedNum = (convertedNum << 1) + (*b - '0');
         b++;
-    }
+	}
 
-    return (convertedNum);
+    	return (convertedNum);
 }
 
